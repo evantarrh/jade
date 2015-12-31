41ebc9f2b623933948769f603e48e983a267b92f
Evan Tarrh <evantarrh@gmail.com>
2015-12-31 14:38:33 -0500

    Adding inline navigation at bottom of page

--- src/js/progress.js
+++ src/js/progress.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
   if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+;
+    document.body.insertAdjacentHTML('afterbegin',
+      '<div id="_progress-bar" style="background-color:#4cb;height:5px;position:fixed;top:0;transition:0.1s ease all;width:0;"></div>');
 
-    var progressBarString = '<div id="_progress-bar" style="background-color:#4cb;height:5px;position:fixed;top:0;transition:0.1s ease all;width:0;"></div>';
-    document.body.insertAdjacentHTML('afterbegin', progressBarString);
     var progressBar = document.getElementById('_progress-bar');
-
     var windowHeight;
     var totalBodyHeight;
     var scrollPosition;
