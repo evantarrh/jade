6df17c408f1cb46eb7867ef33aa6494900c002db
Evan Tarrh <evantarrh@gmail.com>
2016-01-23 20:18:59 -0500

    Fix code-block issues and update 'next steps'

---
+++ js/burger.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("burger"),n=document.getElementsByClassName("big-nav")[0],t=!1;toggleMenu=function(){e.classList.toggle("close"),n.classList.toggle("hidden"),t=t?!1:!0},e.addEventListener("click",toggleMenu),document.onkeydown=function(e){t&&27===e.keyCode&&(e.preventDefault(),toggleMenu())}});
